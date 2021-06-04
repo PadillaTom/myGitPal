6e381aba7f17504390731d20128c4bbf0bcb0fc0
Tomas Padilla <padillatomasagustin@gmail.com>
2021-06-04 08:53:58 +0200

    Search Component

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import styled from "styled-components";
 
+import {Search} from "../Components/Search"
 import {UserRFFG, UserCard, Followers} from "../Components/User Related"
 import {Repos} from "../Components/Repos Related"
 
 const Main = () => {
     return (
         <main>
+            <Search></Search>
             <UserRFFG></UserRFFG>  
             <UserCard></UserCard>  
             <Followers></Followers> 
